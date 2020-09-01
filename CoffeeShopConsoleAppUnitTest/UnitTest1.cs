269df43fbf898ce1de70bde96d8257b7589fcb12
zentarg <apg.chili@gmail.com>
2020-09-01 16:46:20 +0200

    Completes assignments b-e

--- CoffeeShopConsoleAppUnitTest/UnitTest1.cs
+++ CoffeeShopConsoleAppUnitTest/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System;
+using CoffeeShopConsoleApp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CoffeeShopConsoleAppUnitTest
@@ -6,8 +8,76 @@ namespace CoffeeShopConsoleAppUnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestPriceMethodBlackCoffee()
         {
+            Coffee blackCoffee = new BlackCoffee();
+
+            Assert.AreEqual(20, blackCoffee.price());
+        }
+        [TestMethod]
+        public void TestPriceMethodLatte()
+        {
+            Coffee latte = new Latte();
+
+            Assert.AreEqual(40, latte.price());
+        }
+        [TestMethod]
+        public void TestPriceMethodCortado()
+        {
+            Coffee cortado = new Cortado();
+
+            Assert.AreEqual(25, cortado.price());
+        }
+        
+        [TestMethod]
+        public void TestStrengthMethodBlackCoffee()
+        {
+            Coffee blackCoffee = new BlackCoffee();
+
+            Assert.AreEqual("Strong", blackCoffee.Strength());
+        }
+        [TestMethod]
+        public void TestStrengthMethodLatte()
+        {
+            Coffee latte = new Latte();
+
+            Assert.AreEqual("Weak", latte.Strength());
+        }
+        [TestMethod]
+        public void TestStrengthMethodCortado()
+        {
+            Coffee cortado = new Cortado();
+
+            Assert.AreEqual("Medium", cortado.Strength());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDiscountMethodExceptionBlackCoffee()
+        {
+            Coffee test = new BlackCoffee(6);
+            Assert.Fail();
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDiscountMethodExceptionCortado()
+        {
+            Coffee test = new Cortado(6);
+            Assert.Fail();
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDiscountMethodExceptionFlatWhite()
+        {
+            Coffee test = new FlatWhite(6);
+            Assert.Fail();
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDiscountMethodExceptionLatte()
+        {
+            Coffee test = new Latte(6);
+            Assert.Fail();
         }
     }
 }
